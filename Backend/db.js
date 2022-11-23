dd764cf34079f0836625f1978acb9e2a8751684a
Malcolm Gibbons <malcolm@texel.com.ar>
2022-11-23 19:02:56 -0300

    terminando funcionalidades principales

---
+++ Backend/db.js
@@ -0,0 +1,9 @@
+const mysql = require("@mysql/xdevapi");
+
+const config = {
+    password: 'passwd',
+    user: 'root',
+    host: 'localhost',
+    port: 33060,
+    schema: 'mySchema'
+}
--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,13 @@
 let arrayTareas = [
     {
+        id:0,
         nombre: "Estudiar",
         tarea: "Tengo tareas que estudiar",
         fechaDeCreacion: "10/12/1997",
         fechaLimite: "12/12/2002"
     },
     {
+        id:1,
         nombre: "Entrenar",
         tarea: "Tengo que entrenar",
         fechaDeCreacion: "10/10/1999",
@@ -13,10 +15,70 @@ let arrayTareas = [
     }
 ]
 
+let tareaUsuario = () => {
+    let id = arrayTareas.length == 0 ? 0: arrayTareas[arrayTareas.length-1].id + 1
+        
+    let nombre = document.querySelector("#nombre").value
+    
+    let tarea = document.querySelector("#tarea").value
+    
+    let fechaLimite = document.querySelector("#fechaLimite")
+    fechaLimite = new Date(fechaLimite.value)
+    fechaLimite.setDate(fechaLimite.getDate()+1)
+    fechaLimite = fechaLimite.toLocaleDateString()
+
+    const tiempoTranscurrido = Date.now();
+    const hoy = new Date(tiempoTranscurrido).toLocaleDateString();
+
+    return {
+            id:id,
+            nombre: nombre,
+            tarea: tarea,
+            fechaDeCreacion:hoy ,
+            fechaLimite: fechaLimite 
+    }
+}
+
+
+const renderizarTareas = (elementoHTML, array)=>{
+    elementoHTML.innerHTML = ""
+
+    array.forEach( el => {
+
+        elementoHTML.innerHTML += 
+        `<li>
+            <h3>${el.nombre}</h3>
+            <p>${el.tarea}</p>
+            <p>Fecha de creacion: ${el.fechaDeCreacion}</p>
+            <p>Fecha Limite: ${el.fechaLimite}</p>
+            <button onclick="eliminarTarea(${el.id})">Eliminar tarea </button>
+            <button onclick="modificarTarea(${el.id})">Modificar tarea </button>
+        </li>`
+
+    })
+}
+
+
+const modificarTarea = (id) => {
+
+    let ulTareas = document.querySelector(".listaTareas")
+    elementoEnccontrado = arrayTareas.find( el => el.id == id)
+    let index = arrayTareas.findIndex( el => el.id == id)
+    arrayTareas[index] = tareaUsuario()
+    renderizarTareas(ulTareas, arrayTareas)    
+    
+}
+
+const eliminarTarea = (id) => {
+
+    let ulTareas = document.querySelector(".listaTareas")
+    elementoEnccontrado = arrayTareas.find( el => el.id == id)
+    let index = arrayTareas.findIndex( el => el.id == id)
+    arrayTareas.splice(index,1)
+    renderizarTareas(ulTareas, arrayTareas)  
+}
 
 window.addEventListener( "load" , () =>{
-
-    let $ = document.querySelector
 
     let bottonAgregar = document.querySelector(".btnAgregar")
     let ulTareas = document.querySelector(".listaTareas")
@@ -26,46 +88,12 @@ window.addEventListener( "load" , () =>{
         e.preventDefault()
     })
     
-    const renderizarTareas = ()=>{
-
-        ulTareas.innerHTML = ""
-
-        arrayTareas.forEach( el => {
-    
-            ulTareas.innerHTML += `<li>
-                <h3>${el.nombre}</h3>
-                <p>${el.tarea}</p>
-                <p>Fecha de creacion: ${el.fechaDeCreacion}</p>
-                <p>Fecha Limite: ${el.fechaLimite}</p>
-            </li>`
-
-        })
-    }
-
     bottonAgregar.addEventListener( "click", ()=> {
 
-        let nombre = document.querySelector("#nombre")
-        let tarea = document.querySelector("#tarea")
-        let fechaLimite = document.querySelector("#fechaLimite")
-        const tiempoTranscurrido = Date.now();
-        const hoy = new Date(tiempoTranscurrido);
+        arrayTareas.push(tareaUsuario())
 
-
-        arrayTareas.push(
-            {
-                nombre: nombre.value,
-                tarea: tarea.value,
-                fechaDeCreacion: `${hoy.getDate()}/${hoy.getMonth() + 1}/${hoy.getFullYear()}`,
-                fechaLimite: fechaLimite.value
-            }
-        )
-
-        renderizarTareas()
-
+        renderizarTareas(ulTareas, arrayTareas)
     })
-
-
-    renderizarTareas()
-
-
+    
+    renderizarTareas(ulTareas, arrayTareas)
 })
