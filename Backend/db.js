c99599893fe599e726fd6d594cdbc8e6d9535508
Malcolm Gibbons <malcolm@texel.com.ar>
2022-11-24 16:35:31 -0300

    agregando acceso a la bd

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,41 @@
 const mysql = require("@mysql/xdevapi");
 
 const config = {
-    password: 'passwd',
-    user: 'root',
+    password: 'mnH4mvt9We4MryPquaja',
+    user: 'malcolm',
     host: 'localhost',
     port: 33060,
-    schema: 'mySchema'
+    schema: 'mydb',
+    connectTimeout: 10000,
 }
+
+let datosDB = []
+let nombreCol = ["nombreTarea", "tarea", "fechaCreacion", "fechaLimite"]
+let values = ["Estudiar", "Estudiar progrmacion", "1997-12-11", "1997-12-11"]
+
+    mysql.getSession(config)
+        .then( ( session )  => {
+            return session.getSchema("mydb").getTable("tareas")
+        })
+        .then( table => {
+            table.insert(nombreCol).values(values).execute()
+        })
+        .catch( err => {
+            console.log("A ocurrido un error: " + err.message)
+        })
+
+    mysql.getSession(config)
+        .then( ( session )  => {
+            return session.getSchema("mydb").getTable("tareas")
+        })
+        .then( table => {
+            table.select().execute()
+            .then( tareas => {
+                tareas.fetchAll()
+            })
+        })
+        .catch( err => {
+            console.log("A ocurrido un error: " + err.message)
+        })
+
+
